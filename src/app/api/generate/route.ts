1ebfba4dcdaae79af9014071f30b6eeafe3fc771
Petermwansa <mwansapeter800@gmail.com>
2025-08-07 22:40:13 +0300

    Change the api key name

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -45,15 +45,3 @@ export async function POST(req: Request) {
     );
   }
 }
-
-
-
-// import OpenAI from "openai";
-// const client = new OpenAI();
-
-// const response = await client.responses.create({
-//   model: 'gpt-5',
-//   input: "Write a one-sentence bedtime story about a unicorn."
-// })
-
-// console.log(response.output_text);
